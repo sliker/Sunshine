ab9af6b7fb4f36a1bd0eab080714670c5f13992d
David Velasquez <zero.blend@gmail.com>
2015-06-04 07:18:12 -0500

    Progress in lesson 4b

--- app/src/main/java/co/davidvu/sunshine/app/ForecastFragment.java
+++ app/src/main/java/co/davidvu/sunshine/app/ForecastFragment.java
@@ -86,106 +86,6 @@ public class ForecastFragment extends Fragment {
         updateWeather();
     }
 
-    public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
-        @Override
-        protected void onPostExecute(String[] strings) {
-            if (strings != null) {
-                mForecastAdapter.clear();
-                for (int i = 0; i < strings.length; i++) {
-                    mForecastAdapter.add(strings[i]);
-                }
-            }
-        }
-
-        @Override
-        protected String[] doInBackground(String... city) {
-            if (city.length == 0) {
-                return new String[]{""};
-            }
-
-            // These two need to be declared outside the try/catch
-            // so that they can be closed in the finally block.
-            HttpURLConnection urlConnection = null;
-            BufferedReader reader = null;
-
-            // Will contain the raw JSON response as a string.
-            String forecastJsonStr = null;
-
-            try {
-                // Construct the URL for the OpenWeatherMap query
-                // Possible parameters are avaiable at OWM's forecast API page, at
-                // http://openweathermap.org/API#forecast
-                Uri.Builder builder = new Uri.Builder();
-                builder.scheme("http")
-                        .authority("api.openweathermap.org")
-                        .appendPath("data")
-                        .appendPath("2.5")
-                        .appendPath("forecast")
-                        .appendPath("daily")
-                        .appendQueryParameter("q", city[0])
-                        .appendQueryParameter("mode", "json")
-                        .appendQueryParameter("units", mMetrics)
-                        .appendQueryParameter("cnt", "7");
-//                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=Medellin&mode=json&units=metric&cnt=7");
-
-                URL url = new URL(builder.build().toString());
-
-                // Create the request to OpenWeatherMap, and open the connection
-                urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setRequestMethod("GET");
-                urlConnection.connect();
-
-                // Read the input stream into a String
-                InputStream inputStream = urlConnection.getInputStream();
-                StringBuffer buffer = new StringBuffer();
-                if (inputStream == null) {
-                    // Nothing to do.
-                    return new String[]{""};
-                }
-                reader = new BufferedReader(new InputStreamReader(inputStream));
-
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
-                    // But it does make debugging a *lot* easier if you print out the completed
-                    // buffer for debugging.
-                    buffer.append(line + "\n");
-                }
-
-                if (buffer.length() == 0) {
-                    // Stream was empty.  No point in parsing.
-                    return new String[]{""};
-                }
-                forecastJsonStr = buffer.toString();
-
-                try {
-                    return getWeatherDataFromJson(forecastJsonStr, 7);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-            } catch (IOException e) {
-                Log.e(LOG_TAG, "Error ", e);
-                // If the code didn't successfully get the weather data, there's no point in attemping
-                // to parse it.
-                return null;
-            } finally{
-                if (urlConnection != null) {
-                    urlConnection.disconnect();
-                }
-                if (reader != null) {
-                    try {
-                        reader.close();
-                    } catch (final IOException e) {
-                        Log.e(LOG_TAG, "Error closing stream", e);
-                    }
-                }
-            }
-
-            return new String[]{""};
-        }
-    }
-
     /* The date/time conversion code is going to be moved outside the asynctask later,
          * so for convenience we're breaking it out into its own method now.
          */
@@ -287,7 +187,7 @@ public class ForecastFragment extends Fragment {
 
         Log.d(LOG_TAG, "Location: " + location);
         Log.d(LOG_TAG, "Metrics: " + mMetrics);
-        FetchWeatherTask weatherDataTask = new FetchWeatherTask();
+        FetchWeatherTask weatherDataTask = new FetchWeatherTask(getActivity(), mForecastAdapter);
         weatherDataTask.execute(location, null, null);
     }
 
